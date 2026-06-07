4cd47b680823a29d6883f30468ab1f33224be178
Abhinandanporwal <145247297+Abhinandanporwal@users.noreply.github.com>
2026-06-07 17:06:58 +0530

    trying the tut

---
+++ service/app.js
@@ -0,0 +1,19 @@
+class Employee {
+    constructor(name, role, salary) {
+        this.name = name;
+        this.role = role;
+        this.salary = salary;
+    }
+
+    displayDetails() {
+        console.log(
+            `Name: ${this.name}, Role: ${this.role}, Salary: ₹${this.salary}`
+        );
+    }
+}
+
+const employees = [
+    new Employee("Abhinandan", "Frontend Developer", 80000),
+    new Employee("Rahul", "Backend Developer", 75000),
+    new Employee("Priya", "UI/UX Designer", 70000),
+];
